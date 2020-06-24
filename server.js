9d77eaa778916fcaa2cb56bcb5af4a3cbfb50590
Manuel Nuñez <03.manu@gmail.com>
2020-06-24 23:12:24 +0200

    Added comments

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors()) //RESOLVE! Request header field Authorization is not allowed by Access-Control-Allow-Headers in preflight response
 app.use(express.json())
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
